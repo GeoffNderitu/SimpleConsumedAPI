81562023454e26269fa01f8a174516cec59eadff
GeoffNderitu <geoffreynderitun@gmail.com>
2022-11-28 09:09:30 +0300

    FirstConsumedAPI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import "./Main.css"
 import Register from './components/Register';
-import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    // <Router>
+    //  </Router> 
     <div className="App">
-      <NavBar/>
       <Register/>
     </div>
-    </Router>
   );
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,49 @@
 import React from "react";
+import { useState } from "react";
 import "../Main.css";
 const Register = () =>{
+    const [data, setData]=useState({
+        email: "",
+        password: ""
+    })
+
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+
+      const handleChange = (e) => {
+        const {name, value} = e.target;
+        setData((prev) => {
+            return {...prev, [name]: value}
+        })
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        
+        fetch('http://localhost:4000/register',{
+         method: 'POST',
+         headers: {"content-type": "application/json"},
+         body: JSON.stringify(data)
+        }).then(response => response.json()
+            //()=>{
+            //console.log('new user added'); 
+        //  history.push('/');
+        //}
+        )
+        //.then(error => alert(error.statu));
+        .then(result => alert(JSON.stringify(result.error.message, null, 2)));
+    }
 
     return(
         <div className="Main_body">
-            <form action="" className="Regist">
+            <form action="POST" className="Regist">
                 <div className="form_head">
                     <h5>REGISTER</h5>
                 </div>
-                <input type="text" className="input1" placeholder="UserName" required/>
-                <input type="email" className="input2" placeholder="Email" required/>
-                <input type="password" className="input3" placeholder="Password" required/>
-                <button className="tuma" type="submit">Sign Up</button>
+                {/* <input type="text" className="input1" placeholder="UserName" required/> */}
+                <input type="email" className="input2" placeholder="Email" name="email" required onChange={handleChange}/>
+                <input type="password" className="input3" placeholder="Password" name="password" required onChange={ handleChange }/>
+                <button className="tuma" type="submit" onClick={handleSubmit}>Sign Up</button>
             </form>
         </div>
     )
